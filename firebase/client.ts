afcfc7c752d4711a500fb073eb6ac4f2d75a5f4e
mskchaithanyaraj <mskchaithanyaraj@gmail.com>
2025-05-27 12:03:10 +0530

    Security issues

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { initializeApp, getApp, getApps } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBeI4Q6KzN1WKroySFLRmtCYGd3Vutrf6U",
-  authDomain: "acemock-18b7a.firebaseapp.com",
-  projectId: "acemock-18b7a",
-  storageBucket: "acemock-18b7a.firebasestorage.app",
-  messagingSenderId: "760635987723",
-  appId: "1:760635987723:web:e163bfce70ac416f71f40e",
-  measurementId: "G-BZSCQBC5Q3",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
